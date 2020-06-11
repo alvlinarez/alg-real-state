1b0399ed93b4a2a5fe7f74276ffa2a10ffcc0389
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-06-10 21:00:24 -0500

    Logo added

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Navigation from './Navigation';
 
 const Header = () => {
+  const { logo } = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo.svg" }) {
+        publicURL
+      }
+    }
+  `);
   return (
     <header
       css={css`
@@ -16,14 +23,16 @@ const Header = () => {
           max-width: 120rem;
           margin: 0 auto;
           text-align: center;
-          @media(min-width: 768px) {
+          @media (min-width: 768px) {
             display: flex;
             align-items: center;
             justify-content: space-between;
           }
         `}
       >
-        <Link to={'/'}>Real State</Link>
+        <Link to={'/'}>
+          <img src={logo.publicURL} alt="logo"/>
+        </Link>
         <Navigation />
       </div>
     </header>
